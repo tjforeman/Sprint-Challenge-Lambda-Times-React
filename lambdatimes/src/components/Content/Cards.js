2d3a5552b8824e5ea9ca75c4f010e948dc28672d
tjforeman <tylerforeman92@gmail.com>
2019-03-29 12:52:54 -0400

    content rendering

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -6,6 +6,12 @@ const Cards = props => {
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+          {props.cards.map(props =>{
+          return (
+            <Card card={props}/>
+          )
+          }
+          )}
     </div>
   )
 }
